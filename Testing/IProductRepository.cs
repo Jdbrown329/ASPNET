3f19e1b77f685aabd1eb1af7a5e6e97fc4d0668d
Jordan Brown <j.d.brown329@gmail.com>
2022-10-27 10:08:17 -0500

    SecondModuleComplete

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+using Dapper;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+    }
+
+    
+}
